9bede6aec56632df202cb60974374b5a4b14693b
xujl <56443398@qq.com>
2019-12-31 22:20:53 +0800

    rename files

--- build.js
+++ build.js
@@ -7,8 +7,8 @@ const tailwind = require('tailwindcss')
 const filename = 'tailwind'
 const args = process.argv.slice(2)
 const config = args[0] || path.join(__dirname, 'tailwind.config.js')
-const inputFile = path.join(__dirname, `${filename}.css`)
-const outputFile = path.join(__dirname, `dist/${filename}.css`)
+const inputFile = args[1] || path.join(__dirname, `${filename}.src.css`)
+const outputFile = args[2] || path.join(__dirname, `dist/${filename}.css`)
 
 fs.readFile(inputFile, (err, css) => {
   if (err) throw err
